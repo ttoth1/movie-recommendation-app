dda82f7d1938f36b238211ad2537e559331a4eb0
Trevor Toth <trevor.toth11@gmail.com>
2022-04-24 00:59:39 -0400

    Added TMDB API integration so it prints trending movie info to console

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:movie_recommendation_app/widgets/my_navbar.dart';
+import 'package:movie_recommendation_app/secrets/secrets.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List trendingMovies = [];
+  final String apiKey = mySecretKey;
+  final String readAccessToken = myToken;
+
+  @override
+  void initState() {
+    loadMovies();
+    super.initState();
+  }
+
+  loadMovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apiKey, readAccessToken),
+        logConfig: const ConfigLogger(showLogs: true, showErrorLogs: true));
+    Map trendingResult = await tmdbWithCustomLogs.v3.trending
+        .getTrending(mediaType: MediaType.movie);
+    setState(() {
+      trendingMovies = trendingResult['results'];
+    });
+    print(trendingMovies);
+  }
 
   @override
   Widget build(BuildContext context) {
